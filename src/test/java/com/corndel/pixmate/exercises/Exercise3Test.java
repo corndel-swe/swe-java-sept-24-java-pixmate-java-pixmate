6530600498b000dc107df0bc6ac0db0ec7a265ee
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:36:50 +0100

    Update Exercise3Test.java

--- src/test/java/com/corndel/pixmate/exercises/Exercise3Test.java
+++ src/test/java/com/corndel/pixmate/exercises/Exercise3Test.java
@@ -15,34 +15,34 @@ public class Exercise3Test {
         new int[] { 19, 10, 11 },
         new int[] { 9, 5, 105 }
     };
-    assertEquals(gridLimit(grid1), 4);
+    assertEquals(4, gridLimit(grid1));
 
     var grid2 = new int[][] {
         new int[] { 15, 20, 25 },
         new int[] { 11, 12, 13 },
         new int[] { 16, 17, 18 }
     };
-    assertEquals(gridLimit(grid2), 9);
+    assertEquals(9, gridLimit(grid2));
   }
 
   // should return 0 for a grid with all numbers 10 or less
   @Test
   void allLessThanTen() {
     var grid = new int[][] { new int[] { 1, 2 }, new int[] { 3, 4, 5, 6 }, new int[] { 7, 8 }, new int[] { 9 } };
-    assertEquals(gridLimit(grid), 0);
+    assertEquals(0, gridLimit(grid));
   }
 
   // should handle an empty grid
   @Test
   void empty() {
-    assertEquals(gridLimit(new int[][] {}), 0);
+    assertEquals(0, gridLimit(new int[][] {}));
   }
 
   // should handle grids with empty sub-arrays
   @Test
   void handleEmptySubArrays() {
     var grid = new int[][] { new int[] {}, new int[] { 11, 20, 30 }, new int[] {} };
-    assertEquals(gridLimit(grid), 3);
+    assertEquals(3, gridLimit(grid));
   }
 
   // should handle grids with mixed negative and positive numbers
@@ -53,6 +53,6 @@ public class Exercise3Test {
         new int[] { 40, 50, -60 },
         new int[] { -70, 80, 90 }
     };
-    assertEquals(gridLimit(grid), 5);
+    assertEquals(5, gridLimit(grid));
   }
 }
