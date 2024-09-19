5635addda6521dacd695970381addb6ccdd8fead
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:50:59 +0100

    Update Exercise2a.java

--- src/main/java/com/corndel/pixmate/exercises/Exercise2a.java
+++ src/main/java/com/corndel/pixmate/exercises/Exercise2a.java
@@ -1,15 +1,19 @@
 package com.corndel.pixmate.exercises;
 
+/**
+ * @see <a href="https://tech-docs.corndel.com/java/loop-control-flow.html">Loop Control Flow in Java</a>
+ */
 public class Exercise2a {
-  // https://tech-docs.corndel.com/java/loop-control-flow.html
 
   /**
-   * Calculates the sum of numbers in an array that are either even or greater than 10, but skips
-   * numbers that are both.
-   *
-   * @example // returns 37 sumSelective([1, 4, 12, 15, 20, 5, 8, 10])
-   * @param {int[]} numbers - An array of numbers to be processed.
-   * @returns {int} The sum of numbers that meet the criteria.
+   * Calculates the sum of numbers in an array that are either even or greater than 10,
+   * but skips numbers that are both.
+   * <p>
+   * For example, if the input array is [1, 4, 12, 15, 20, 5, 8, 10], the method returns 37.
+   * </p>
+   * 
+   * @param numbers an array of numbers to be processed
+   * @return the sum of numbers that meet the criteria
    */
   public static int sumSelective(int[] numbers) {
     // TODO
