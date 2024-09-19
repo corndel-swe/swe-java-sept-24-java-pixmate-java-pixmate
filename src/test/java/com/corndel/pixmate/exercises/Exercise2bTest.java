eafb8c0b7965f5b75fe6de8572ffe7ca90b8b21b
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:36:27 +0100

    Update Exercise2bTest.java

--- src/test/java/com/corndel/pixmate/exercises/Exercise2bTest.java
+++ src/test/java/com/corndel/pixmate/exercises/Exercise2bTest.java
@@ -11,39 +11,39 @@ public class Exercise2bTest {
   // predecessor
   @Test
   void test() {
-    assertEquals(stepDown(new int[] { 10, 12, 15, 14, 16, 20 }), 3);
-    assertEquals(stepDown(new int[] { 3, 4, 2, 5, 6 }), 2);
+    assertEquals(3, stepDown(new int[] { 10, 12, 15, 14, 16, 20 }));
+    assertEquals(2, stepDown(new int[] { 3, 4, 2, 5, 6 }));
   }
 
   // should return -1 for an array where no element is smaller than its
   // predecessor
   @Test
   void increasing() {
-    assertEquals(stepDown(new int[] { 5, 10, 15, 20 }), -1);
-    assertEquals(stepDown(new int[] { 1, 2, 3, 4, 5 }), -1);
+    assertEquals(-1, stepDown(new int[] { 5, 10, 15, 20 }));
+    assertEquals(-1, stepDown(new int[] { 1, 2, 3, 4, 5 }));
   }
 
   // should return -1 for an empty array
   @Test
   void empty() {
-    assertEquals(stepDown(new int[] {}), -1);
+    assertEquals(-1, stepDown(new int[] {}));
   }
 
   // should handle arrays with all identical elements
   @Test
   void constant() {
-    assertEquals(stepDown(new int[] { 7, 7, 7, 7 }), -1);
+    assertEquals(-1, stepDown(new int[] { 7, 7, 7, 7 }));
   }
 
   // should handle arrays where the first two elements satisfy the condition
   @Test
   void firstTwo() {
-    assertEquals(stepDown(new int[] { 3, 2, 4, 5, 6 }), 1);
+    assertEquals(1, stepDown(new int[] { 3, 2, 4, 5, 6 }));
   }
 
   // should return the first index, and not subsequent indicies
   @Test
   void first() {
-    assertEquals(stepDown(new int[] { 5, 4, 3, 2, 1 }), 1);
+    assertEquals(1, stepDown(new int[] { 5, 4, 3, 2, 1 }));
   }
 }
