e821d7cd700fc16a4f3ced78c8a37673d200012e
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:36:03 +0100

    Update Exercise2aTest.java

--- src/test/java/com/corndel/pixmate/exercises/Exercise2aTest.java
+++ src/test/java/com/corndel/pixmate/exercises/Exercise2aTest.java
@@ -11,37 +11,37 @@ public class Exercise2aTest {
   // should sum numbers that are either even or greater than 10, but not both
   @Test
   void typical() {
-    assertEquals(sumSelective(new int[] { 1, 4, 12, 15, 20, 5, 8, 10 }), 37);
+    assertEquals(37, sumSelective(new int[] { 1, 4, 12, 15, 20, 5, 8, 10 }));
   }
 
   // should return 0 for an empty array
   @Test
   void empty() {
-    assertEquals(sumSelective(new int[] {}), 0);
+    assertEquals(0, sumSelective(new int[] {}));
   }
 
   // should correctly handle arrays with only even numbers
   @Test
   void even() {
-    assertEquals(sumSelective(new int[] { 2, 4, 6, 8, 10 }), 30);
+    assertEquals(30, sumSelective(new int[] { 2, 4, 6, 8, 10 }));
   }
 
   // should correctly handle arrays with numbers only greater than 10
   @Test
   void allGreaterThanTen() {
-    assertEquals(sumSelective(new int[] { 11, 13, 15 }), 39);
+    assertEquals(39, sumSelective(new int[] { 11, 13, 15 }));
   }
 
   // should skip numbers that are both even and greater than 10
   @Test
   void skipBothEvenAndGreaterThanTen() {
-    assertEquals(sumSelective(new int[] { 12, 14, 16, 3, 5 }), 0);
+    assertEquals(0, sumSelective(new int[] { 12, 14, 16, 3, 5 }));
   }
 
   // should handle large arrays
   @Test
   void handlesLargeArrays() {
     var largeArray = IntStream.range(1, 50).toArray();
-    assertEquals(sumSelective(largeArray), 630);
+    assertEquals(630, sumSelective(largeArray));
   }
 }
